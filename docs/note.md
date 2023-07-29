10c05ead571724958bd9b18e7559e483d310c012
Vagner H <vh3rd7@gmail.com>
2023-07-29 15:47:38 -0300

    feat: add 'header' component

--- docs/note.md
+++ docs/note.md
@@ -6,3 +6,12 @@
 `yarn add -D eslint prettier eslint-plugin-prettier eslint-config-prettier @typescript-eslint/eslint-plugin@latest @typescript-eslint/parser@latest`
 
 `npx eslint --init`
+
+### create header component
+
+[component-overview](https://angular.io/guide/component-overview)  
+command:
+`ng g c component/template/header`
+
+add MatToolbar
+[material-toolbar](https://material.angular.io/components/toolbar/overview)
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './component/template/header/header.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  declarations: [AppComponent, HeaderComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
